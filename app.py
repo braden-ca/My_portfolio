2376a095ff795796265bd0865bba54e47ae9102b
smelserb1 <bsmelser@cs.csub.edu>
2025-02-16 18:57:39 -0800

    Created a list of objects

--- app.py
+++ app.py
@@ -2,11 +2,24 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
+projects = [
+    {
+        "title": "SpareChange - Real Estate App",
+        "description": "A Flask-based real estate listing platform.",
+        "tech": "Flask, Python, SQLite, Firebase",
+        "link": "https://solaire.cs.csub.edu/gitlab/sparechange/sparechange/-/tree/main/backend?ref_type=heads"
+    },
+    {
+        "title": "GoldRushLite - Game",
+        "description": "A game developed in C using Windows X11.",
+        "tech": "C, X11",
+        "link": "https://github.com/braden-ca/Goldrush-Lite"
+    }
+]
 
 @application.route('/projects')
 def project_page():
-    return render_template('projects.html')
-
+    return render_template('projects.html', projects = projects)
 
 @application.route('/')
 def about():
